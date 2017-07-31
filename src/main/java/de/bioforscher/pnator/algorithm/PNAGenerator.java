fd2eeb59170fbec089e9069b79623ad25806abc8
Alexander Eisold <eisold@hs-mittweida.de>
2017-07-31 11:10:19 +0200

    ADD NEW CLASS PNAGENERATOR

---
+++ src/main/java/de/bioforscher/pnator/algorithm/PNAGenerator.java
@@ -0,0 +1,11 @@
+package de.bioforscher.pnator.algorithm;
+
+import de.bioforscher.singa.chemistry.parser.pdb.structures.StructureParser;
+import de.bioforscher.singa.chemistry.physical.model.Structure;
+
+
+public class PNAGenerator {
+
+
+
+}
