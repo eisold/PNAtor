b2d0eaba98ec9cd038e6e65b395c4ef187e90431
Christoph Leberecht <leberech@hs-mittweida.de>
2017-08-03 10:38:16 +0200

    implemented algorithm stub and logging

--- src/main/java/de/bioforscher/pnator/algorithm/PNAGenerator.java
+++ src/main/java/de/bioforscher/pnator/algorithm/PNAGenerator.java
@@ -1,13 +1,96 @@
 package de.bioforscher.pnator.algorithm;
 
+import de.bioforscher.singa.chemistry.descriptive.elements.ElementProvider;
 import de.bioforscher.singa.chemistry.parser.pdb.structures.StructureParser;
+import de.bioforscher.singa.chemistry.parser.pdb.structures.StructureWriter;
+import de.bioforscher.singa.chemistry.physical.atoms.Atom;
+import de.bioforscher.singa.chemistry.physical.atoms.AtomName;
+import de.bioforscher.singa.chemistry.physical.atoms.RegularAtom;
+import de.bioforscher.singa.chemistry.physical.leaves.Nucleotide;
+import de.bioforscher.singa.chemistry.physical.model.StructuralEntityFilter;
 import de.bioforscher.singa.chemistry.physical.model.Structure;
+import de.bioforscher.singa.javafx.viewer.ColorScheme;
+import de.bioforscher.singa.javafx.viewer.StructureViewer;
+import de.bioforscher.singa.mathematics.vectors.Vector3D;
+import de.bioforscher.singa.mathematics.vectors.Vectors3D;
+import javafx.application.Application;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class PNAGenerator {
 
+    private static final Logger logger = LoggerFactory.getLogger(PNAGenerator.class);
+
+    private static int addedAtomIndex = 1000;
+
+    public static void main(String[] args) {
+
         Structure structure = StructureParser.online()
                 .pdbIdentifier("1BNA")
                 .parse();
+
+        logger.info("Parsing structure {}.", structure.getPdbIdentifier());
+
+        List<Nucleotide> nucleotides = structure.getAllLeaves().stream()
+                .filter(StructuralEntityFilter.LeafFilter.isNucleotide())
+                .map(leaf -> (Nucleotide) leaf)
+                .collect(Collectors.toList());
+
+        logger.info("Collected {} nucleotides.", nucleotides.size());
+
+        nucleotides
+                .forEach(nucleotide -> {
+                    Atom atomOP1 = null;
+                    if (nucleotide.containsAtomWithName(AtomName.OP1)) {
+                        atomOP1 = nucleotide.getAtomByName(AtomName.OP1);
+                    }
+                    Atom atomOP2 = null;
+                    if (nucleotide.containsAtomWithName(AtomName.OP2)) {
+                        atomOP2 = nucleotide.getAtomByName(AtomName.OP2);
+                    }
+                    Atom atomP = null;
+                    if (nucleotide.containsAtomWithName(AtomName.P)) {
+                        atomP = nucleotide.getAtomByName(AtomName.P);
+                    }
+                    if (atomOP1 != null && atomOP2 != null && atomP != null) {
+                        Vector3D positionOP1 = atomOP1.getPosition();
+                        Vector3D positionOP2 = atomOP2.getPosition();
+
+                        Vector3D centroid = Vectors3D.getCentroid(Arrays.asList(positionOP1, positionOP2));
+                        logger.trace("Calculated centroid between {} and {} as {}.",positionOP1, positionOP2,
+                                centroid);
+
+                        int integer = nucleotide.addNode(new RegularAtom(addedAtomIndex++, ElementProvider.CARBON,
+                                "C4'", centroid));
+
+                        structure.getFirstModel().get().removeNode(atomOP1);
+                        structure.getFirstModel().get().removeNode(atomOP2);
+                    } else {
+                        logger.warn("Could not calculate backbone for nucleotide {}.", nucleotide);
+                    }
+
+                });
+
+        try {
+            StructureWriter.writeBranchSubstructure(structure.getFirstModel().get(), Paths.get("/tmp/test.pdb"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        StructureViewer.colorScheme = ColorScheme.BY_ELEMENT;
+        StructureViewer.structure = structure;
+        Application.launch(StructureViewer.class);
+
+
+    }
+
+
 
 }
